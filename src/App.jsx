e2c7989f078dafaa8f3fb760cc895494cc1bd4d2
antonioamate <4252008@alu.murciaeduca.es>
2023-12-04 20:14:19 +0100

    completado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
-import MiPrimerComponente  from './componentes/MiPrimerComponente'
-function App() {
+import Parte from './componentes/Parte'
+
+const App = () => {
+  const curso = 'Desarrollo web en entorno cliente. 2ª Evaluación'
+  const parte1 = 'Fundamentos de React'
+  const ejercicios1 = 10
+  const parte2 = 'React avanzado'
+  const ejercicios2 = 7
+  const parte3 = 'Otros frameworks'
+  const ejercicios3 = 14
 
   return (
     <div>
-        <MiPrimerComponente nombre="Antonio"
-                            apellidos="Amate Carrión"
-                            edad="27"
-        ></MiPrimerComponente>
+      <h1>{curso}</h1>
+      <Parte parte={parte1} ejercicios={ejercicios1}/>
+      <Parte parte={parte2} ejercicios={ejercicios2}/>
+      <Parte parte={parte3} ejercicios={ejercicios3}/>
+      <p>Numero de ejercicios {ejercicios1 + ejercicios2 + ejercicios3}</p>
     </div>
   )
 }
---
+++ src/componentes/Parte.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Parte(props) {
+    return ( <p>
+        {props.parte} {props.ejercicios}
+      </p>
+    )
+}
+export default Parte
